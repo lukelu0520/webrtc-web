f48c30381220a56ffe267a1c480a6f2f22b43913
lukelu0520 <appl.chlu@gmail.com>
2020-12-07 20:31:32 -0800

    put pcConfig into RTCPeerConnection

--- work/js/main.js
+++ work/js/main.js
@@ -150,7 +150,7 @@ window.onbeforeunload = function() {
 
 function createPeerConnection() {
   try {
-    pc = new RTCPeerConnection(null);
+    pc = new RTCPeerConnection(pcConfig);
     pc.onicecandidate = handleIceCandidate;
     pc.onaddstream = handleRemoteStreamAdded;
     pc.onremovestream = handleRemoteStreamRemoved;
